553b85aac55b8f4bf326e6912043d8ecfd613735
MC Kcire Jimenez <erick_larebel.man@hotmail.com>
2023-03-23 20:43:44 -0600

    Primer intento

---
+++ Loteria/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace Loteria
+{
+    partial class Form1
+    {
+        string[] archivos = new string[54];
+        int[] carta = new int[16];
+        int[] acertadas = new int[10];
+        int intentos = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            dgv.Visible = false;
+            pictureBox1.Visible = false;
+            button1.Visible = false;
+            label1.Visible = false;
+            //Lenamos un arreglo con el nombre de las imagenes que disponemos
+            for (int i = 0; i < archivos.Length; i++)
+                archivos[i] = "imagen" + (i + 1).ToString() + ".jpg";
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Boton Empezar a Jugar
+            dgv.RowCount = 3;
+
+            int i, j, k = 0, n = 9;
+            //Metodo nos devuelve un arreglo de numeros diferentes
+            carta = AleatoriosDiferentes(n);
+            //Mostramos las imagenes en el DataGridView
+            for (i = 0; i < dgv.RowCount; i++)
+                for (j = 0; j < dgv.ColumnCount; j++)
+                {
+                    dgv[j, i].Value = Image.FromFile("imagen" + carta[k] + ".jpg");
+                    k++;
+
+                }
+            for (i = 0; i < dgv.RowCount; i++)
+                dgv.Rows[i].Height = 100;
+
+            dgv.Visible = true;
+            pictureBox1.Visible = true;
+            button2.Visible = true;
+            label1.Visible = true;
+            button1.Text = "Volver a jugar";
+            intentos = 0;
+            tbintentos.Text = "";
+            //Ponemos en el arreglo acertadas 0 ya que es el que nos va ayudar
+            //A Determinar si ya competo la carta
+            for (int m = 0; m < acertadas.Length; m++)
+                acertadas[m] = 0;
+            pictureBox1.Image = Image.FromFile("imageninicio.jpg");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Boton Cantar
+            intentos++;
+            tbintentos.Text = intentos.ToString();
+
+            Random r = new Random();
+            int i, j, k, a;
+            int bc = r.Next(1, 21);
+
+
+            pictureBox1.Image = Image.FromFile(archivos[bc - 1]);
+            k = 0;
+            //Verificamos si la imagen del picturebox esta en el datagrid
+            //Si es asi ponemos la imagen por default y al arreglo acertados ponemos 1
+            for (i = 0; i < dgv.RowCount; i++)
+            {
+                for (j = 0; j < dgv.ColumnCount; j++)
+                {
+                    if (bc == carta[k])
+                    {
+                        acertadas[k] = 1;
+                        dgv[j, i].Value = Image.FromFile("imagendefault.png");
+                    }
+                    k++;
+                }
+            }
+            int suma = 0;
+            //Sumamos los elementos de acertadas
+            for (a = 0; a < acertadas.Length; a++)
+                suma += acertadas[a];
+
+            if (suma == acertadas.Length)
+                MessageBox.Show("Carta completa");
+
+        }
+
+
+
+        private int[] AleatoriosDiferentes(int n)
+        {
+            int[] numeros = new int[n];
+            Random r = new Random();
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                bool yaExiste = true;
+                while (yaExiste)
+                {
+                    numeros[i] = r.Next(1, 21);
+                    bool yaEsta = false;
+                    for (int j = 0; j < i; j++)
+                        if (numeros[i] == numeros[j])
+                        {
+                            yaEsta = true;
+                            break;
+                        }
+                    yaExiste = yaEsta;
+                }
+
+            }
+            return numeros;
+        }
+
+        private PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private Label label1;
+        private System.Windows.Forms.Button button2;
+        private DataGridView dgv;
+        private TextBox tbintentos;
+    }
+}
+
---
+++ Loteria/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loteria
+{
+    public partial class Form1 : Form
+    {
+        
+
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.tbintentos = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(128, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(603, 177);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Volver a jugar";
+            this.button1.UseVisualStyleBackColor = true;
+            
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inentos";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(656, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Empezar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(128, 205);
+            this.dgv.Name = "dgv";
+            this.dgv.RowTemplate.Height = 25;
+            this.dgv.Size = new System.Drawing.Size(603, 145);
+            this.dgv.TabIndex = 5;
+            // 
+            // tbintentos
+            // 
+            this.tbintentos.Location = new System.Drawing.Point(13, 42);
+            this.tbintentos.Name = "tbintentos";
+            this.tbintentos.Size = new System.Drawing.Size(65, 23);
+            this.tbintentos.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(771, 426);
+            this.Controls.Add(this.tbintentos);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+    }
+}
